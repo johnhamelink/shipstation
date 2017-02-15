b1ce5190f17cea25e0632ec53e8143cb1d93cdae
John Hamelink <john@johnhamelink.com>
2017-02-15 03:34:05 +0000

    Fix markdown on shipstation.ex

--- lib/shipstation.ex
+++ lib/shipstation.ex
@@ -4,9 +4,9 @@ defmodule Shipstation do
     ship your orders. They aim to streamline shipping for online sellers, no
     matter where they sell their products online.
 
-    This hex package is designed to allow you to integrate your ecommerce product
-    with Shipstation's publicly accessible API
-    (http://www.shipstation.com/developer-api/).
+    This hex package is designed to allow you to integrate your ecommerce
+    product with Shipstation's [publicly accessible
+    API](http://www.shipstation.com/developer-api/).
   """
 
   use HTTPoison.Base
