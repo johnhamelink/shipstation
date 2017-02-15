9458cde07c10f8caed5a4e3cf4b3dbd543d73ee4
John Hamelink <john@johnhamelink.com>
2017-02-15 15:27:24 +0000

    Version 0.2.0:
    * Added authentication support

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Shipstation.Mixfile do
 
   def project do
     [app: :shipstation,
-     version: "0.1.2",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
