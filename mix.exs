b3d92abc07e5af486db3a18010e1c29bbc6f8e5b
John Hamelink <john@johnhamelink.com>
2017-02-16 00:49:30 +0000

    Version 0.3.2:
    * Use Application.spec/2 to retrieve the Application version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Shipstation.Mixfile do
 
   def project do
     [app: :shipstation,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
